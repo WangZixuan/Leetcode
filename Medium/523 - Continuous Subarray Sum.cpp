706017d0fd2bc75abb7eee8c78bad0b5b9c4f65e
Zixuan Wang <wzxllx@gmail.com>
2018-03-15 19:23:46 +0800

    First in 2017/02/26, do not work, but now it works.

--- Medium/523 - Continuous Subarray Sum.cpp
+++ Medium/523 - Continuous Subarray Sum.cpp
@@ -6,63 +6,69 @@ Continuous Subarray Sum
 
 Given a list of non-negative numbers and a target integer k, write a function to check if the array has a continuous subarray of size at least 2 that sums up to the multiple of k, that is, sums up to n*k where n is also an integer.
 Example 1:
-	Input: [23, 2, 4, 6, 7],  k=6
-	Output: True
-	Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
+Input: [23, 2, 4, 6, 7],  k=6
+Output: True
+Explanation: Because [2, 4] is a continuous subarray of size 2 and sums up to 6.
 
 Example 2:
-	Input: [23, 2, 6, 4, 7],  k=6
-	Output: True
-	Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
+Input: [23, 2, 6, 4, 7],  k=6
+Output: True
+Explanation: Because [23, 2, 6, 4, 7] is an continuous subarray of size 5 and sums up to 42.
 
 Note:
-	The length of the array won't exceed 10,000.
-	You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
+The length of the array won't exceed 10,000.
+You may assume the sum of all the numbers is in the range of a signed 32-bit integer.
 
 @author Zixuan
-@date	2017/02/26
+@date	2018/3/15
 */
 
+#include <map>
 #include <vector>
 using namespace std;
 
-class Solution 
+class Solution
 {
 public:
-	bool checkSubarraySum(vector<int>& nums, int k) 
+	//Reference: https://leetcode.com/problems/continuous-subarray-sum/discuss/99499/Java-O(n)-time-O(k)-space
+	bool checkSubarraySum(vector<int>& nums, int k)
 	{
+		if (1 == nums.size())
+			return false;
+		//Deal with k==0
 		if (0 == k)
 		{
-			if (nums.size() < 2)
-				return false;
-			
-			auto sum = 0;
-			for (auto val : nums)
-				sum += val;
-			if (0==sum)
-				return true;
-			
+			for (auto i = 0; i < static_cast<int>(nums.size()) - 1; ++i)
+			{
+				if (0 == nums[i])
+				{
+					if (0 == nums[i + 1])
+						return true;
+				}
+			}
 			return false;
 		}
 
-		if (nums.size() <= 1)//Attention here, 0==k!
-			return false;
+		//[5, 2, 4], 5
+		auto first = 0 == nums[0] % k ? 1 : 0;
+		map<int, int> sumMod;
+		sumMod.insert(make_pair(0, -1));
 
-		vector<int> temp;
-
-		for (auto i = 1; i < nums.size(); ++i)
+		auto sum = 0;
+		for (auto i = first; i < nums.size(); ++i)
 		{
-			temp.push_back(nums[i - 1] % k);
-			for (auto& val : temp)
-				val = (val + nums[i]) % k;
-			if (isAcceptable(temp))
-				return true;
+			auto mod = (sum += nums[i]) % k;
+
+			auto it = sumMod.find(mod);
+			if (it == sumMod.end())
+				sumMod.insert(make_pair(mod, i));
+			else
+			{
+				if (it->second < i)
+					return true;
+			}
 		}
+
 		return false;
-	}
-private:
-	static bool isAcceptable(vector<int>& nums)
-	{
-		return find(nums.begin(), nums.end(), 0) != nums.end();
 	}
 };
